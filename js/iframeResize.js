abf883764c195055024c5184601ff111e2750fb2
druviantllc <druviantllc@gmail.com>
2024-09-17 12:33:50 -0400

    Delete js/iframeResize.js

--- js/iframeResize.js
+++
@@ -1,6 +0,0 @@
-function resizeIframe(iframe) {
-    // Wait for the iframe content to load fully before resizing
-    iframe.onload = function() {
-        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-    };
-}
